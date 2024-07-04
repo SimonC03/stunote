2abe944efe8cbf9e1334dfca352272e1ae947981
SimonC03 <blixxtensimon@gmail.com>
2024-07-04 09:56:25 +0200

    beta

--- build/static/chunks/app/(root)/layout-2b4511c3cd5a9bfd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[332],{3211:function(e,n,s){Promise.resolve().then(s.bind(s,5617)),Promise.resolve().then(s.bind(s,694)),Promise.resolve().then(s.t.bind(s,8173,23)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[999,32,648,138,72,264,971,23,744],function(){return e(e.s=3211)}),_N_E=e.O()}]);
