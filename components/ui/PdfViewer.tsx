5aa74121c3a00170480c94af4162c5b517f25889
SimonC03 <blixxtensimon@gmail.com>
2024-07-05 16:23:27 +0200

    pdf

--- components/ui/PdfViewer.tsx
+++ components/ui/PdfViewer.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-const adobeApiKey = process.env.NEXT_PUBLIC_ADOBE_API_KEY!;
 interface PdfViewerProps {
   pdfUrl: string;
 }
@@ -41,7 +40,7 @@ const PdfViewer: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
       .then(() => {
         if (viewerRef.current) {
           const adobeDCView = new window.AdobeDC.View({
-            clientId: adobeApiKey,
+            clientId: '43c76f66fe814110ae50dd046b84c477',
             divId: viewerRef.current.id,
           });
 
@@ -57,8 +56,8 @@ const PdfViewer: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
               showPageControls: false,
               showDownloadPDF: false,
               showPrintPDF: false,
-              showFullScreen: true,
-              showZoomControl: true,
+              showFullScreen: false,
+              showZoomControl: false,
               hasReadOnlyAccess: true,
             }
           );
