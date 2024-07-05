9f471ac57693aa1d7394688465094fa28147e2a1
SimonC03 <blixxtensimon@gmail.com>
2024-07-05 13:11:59 +0200

    mobil

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -6,19 +6,19 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="flex flex-col items-center justify-center text-center p-4 bg-transparent mt-auto w-full">
-      <div className="flex space-x-20 mb-4">
+      <div className="flex space-x-10 sm:space-x-20 mb-4">
         <Link href="https://www.instagram.com/stunote/" target="_blank" rel="noopener noreferrer">
-          <div className="social-icon">
+          <div className="social-icon w-4 h-4 sm:w-6 sm:h-6">
             <Image src="/icons/instagramLogo.svg" width={25} height={25} alt="Instagram Icon" />
           </div>
         </Link>
         <Link href="https://www.linkedin.com/company/stunote/" target="_blank" rel="noopener noreferrer">
-          <div className="social-icon">
+          <div className="social-icon w-4 h-4 sm:w-6 sm:h-6">
             <Image src="/icons/linkedinLogo.svg" width={25} height={25} alt="LinkedIn Icon" />
           </div>
         </Link>
       </div>
-      <div className="text-xs sm:text-sm md:text-base text-white">
+      <div className="text-xxs sm:text-xs md:text-sm text-white">
         <p>Stunote is not affiliated with or supported by any school, college, or university.</p>
         <p>Copyright © 2024 Stunote</p>
       </div>
@@ -29,6 +29,9 @@ const Footer = () => {
         .social-icon:hover {
           transform: scale(1.2);
           box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        }
+        .text-xxs {
+          font-size: 0.625rem; /* Equivalent to 10px */
         }
       `}</style>
     </footer>
