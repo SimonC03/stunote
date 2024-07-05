b638ccbc5de41c192e891f8f995b82dfb13e9449
SimonC03 <blixxtensimon@gmail.com>
2024-07-05 11:44:49 +0200

    mobilkomp

--- app/(auth)/LandingPage.tsx
+++ app/(auth)/LandingPage.tsx
@@ -22,22 +22,28 @@ const LandingPage: React.FC = () => {
         STUNOTE
       </p>
       </div>
-      <div className="flex flex-col justify-center flex-1 text-left mt-20 ml-6">
-        <h1 className="text-5xl md:text-6xl font-bold mb-4 text-orange-1 w-full max-w-3xl">CREATED BY STUDENTS</h1>
-        <h2 className="text-4xl md:text-5xl font-bold mb-8 w-full max-w-3xl">FOR STUDENTS!</h2>
-        <p className="text-lg md:text-xl mb-8 max-w-2xl">
+      <div className="flex flex-col justify-center flex-1 text-left mt-10 md:mt-20 ml-2 md:ml-6">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-2 md:mb-4 text-orange-1 w-full max-w-3xl">CREATED BY STUDENTS</h1>
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 md:mb-8 w-full max-w-3xl">FOR STUDENTS!</h2>
+        <p className="text-base md:text-lg lg:text-xl mb-4 md:mb-8 max-w-2xl">
           Welcome to StuNote - your ultimate global student platform! We make studying easier by offering high-quality, course-relevant content. Join us today and let’s learn together!
         </p>
         <div className="flex space-x-4 mb-8 ">
           <Link href="/sign-in">
-          <Button variant="default" size="lg">
-              LOGIN
-            </Button>
+          <Button variant="default" className="lg:hidden" size="sm">
+            LOGIN
+          </Button>
+          <Button variant="default" className="hidden lg:block" size="lg">
+            LOGIN
+          </Button>
           </Link>
           <Link href="/sign-up">
-            <Button variant="default" size="lg">
-              SIGN UP
-            </Button>
+          <Button variant="default" className="lg:hidden" size="sm">
+            SIGN UP
+          </Button>
+          <Button variant="default" className="hidden lg:block" size="lg">
+            SIGN UP
+          </Button>
           </Link>
         </div>
       </div>
