a0a1dbf398afbba35faff8458bc6de380f86b165
SimonC03 <blixxtensimon@gmail.com>
2024-07-06 18:49:51 +0200

    vercel_analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { UserProvider } from '../context/UserContext';
 import Toaster from "@/components/ui/Toaster";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,11 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SpeedInsights/>
-          <UserProvider>
-            {children}
-            <Toaster />
-          </UserProvider>
+        <Analytics/>
+          <SpeedInsights/>
+            <UserProvider>
+              {children}
+              <Toaster />
+            </UserProvider>
       </body>
     </html>
   );
