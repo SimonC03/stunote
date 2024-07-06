6a089bbfa4a6a2b1257e6bba0b68796c86a4afec
SimonC03 <blixxtensimon@gmail.com>
2024-07-06 14:58:01 +0200

    speed_insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from '../context/UserContext';
 import Toaster from "@/components/ui/Toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,9 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <UserProvider>
-          {children}
-          <Toaster />
+        <SpeedInsights/>
+          <UserProvider>
+            {children}
+            <Toaster />
           </UserProvider>
       </body>
     </html>
