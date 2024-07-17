14728d219d7f703dd2bd81d11c098c46a9b8bac6
SimonC03 <blixxtensimon@gmail.com>
2024-07-17 09:44:32 +0200

    updt

--- lib/jobs.ts
+++ lib/jobs.ts
@@ -10,11 +10,11 @@ export interface Job {
   
   export const JOBS_DATA: Job[] = [
     {
-        id: 1,
-        title: "Student Ambassador",
-        description: "We are seeking dedicated university/college students to join our team as Course Notes Editors. In this role, you will transcribe your course notes and upload them to our platform. The ideal candidate can take digital notes using an iPad or similar device. Clear and legible handwriting is a plus.",
-        type: "Part-time",
-        location: "Remote",
+      "id": 1,
+      "title": "Student Ambassador",
+      "description": "We are seeking dedicated university/college students to join our team as Student Ambassadors! In this role, you will report the courses you are taking, submit lecture notes, and contribute material for the courses you are enrolled in. In return, you will receive a premium subscription to Stunote, discounts on our used textbooks, and other perks such as movie tickets. The ideal candidate is currently enrolled in a university/college, can take digital notes using an iPad or similar device, and has clear and legible handwriting. Accuracy and readability are highly valued in this role. Please include in your application the name of your school and program, and explain why you believe you are a good fit for this role.",
+      "type": "Part-time",
+      "location": "Remote"
     },
     {
       id: 2,
