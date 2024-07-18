caacb1628f34ea8a37d4ad37d113cef55a5ce0df
SimonC03 <85081292+SimonC03@users.noreply.github.com>
2024-07-18 09:56:55 +0200

    Update Footer.tsx

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import TrustpilotLogo from '/public/icons/trustpilotLogo.svg';  // Anpassa sökvägen till var du har sparat SVG-filen
 
 const Footer = () => {
   return (
@@ -17,30 +18,17 @@ const Footer = () => {
             <Image src="/icons/linkedinLogo.svg" width={25} height={25} alt="LinkedIn Icon" />
           </div>
         </Link>
-       <div className="trustpilot-widget" data-locale="en-US" data-template-id="56278e9abfbbba0bdcd568bc" data-businessunit-id="6698a6bb4f3dfbc3a06f8906" data-style-height="25px" data-style-width="50%">
-          <a href="https://www.trustpilot.com/review/stunote.se" target="_blank" rel="noopener">
-            <Image src="/icons/trustpilotLogo.svg" width={54} height={54} alt="Trustpilot Icon" />
-          </a>
+        <Link href="https://www.trustpilot.com/review/stunote.se" target="_blank" rel="noopener noreferrer">
+          <div className="social-icon w-4 h-4 sm:w-6 sm:h-6">
+            <Image src={TrustpilotLogo} width={25} height={25} alt="Trustpilot Icon" />
+          </div>
+        </Link>
       </div>
+      <div className="text-white text-sm sm:text-base mt-4">
+        © 2023 StuNote. All Rights Reserved.
       </div>
-      <div className="text-xxs sm:text-xs md:text-sm text-white">
-        <p>Stunote is not affiliated with or supported by any school, college, or university.</p>
-        <p>Copyright © 2024 Stunote</p>
-      </div>
-      <style jsx>{`
-        .social-icon {
-          transition: transform 0.3s ease, box-shadow 0.3s ease;
-        }
-        .social-icon:hover {
-          transform: scale(1.2);
-          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-        }
-        .text-xxs {
-          font-size: 0.625rem; /* Equivalent to 10px */
-        }
-      `}</style>
     </footer>
   );
-};
+}
 
 export default Footer;
