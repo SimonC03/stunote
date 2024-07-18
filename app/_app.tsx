fbcefca84d9e255d14f3af3eb6f5e732fbeb5665
SimonC03 <85081292+SimonC03@users.noreply.github.com>
2024-07-18 09:25:40 +0200

    trustpilot

--- app/_app.tsx
+++ app/_app.tsx
@@ -2,9 +2,17 @@
 import React from 'react';
 import { UserProvider } from '../context/UserContext';
 import type { AppProps } from 'next/app';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <Head>
+      <script
+          type="text/javascript"
+          src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
+          async
+      ></script>
+    </Head>
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
