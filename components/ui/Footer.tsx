ccdc08f2e529e6c08f7e142615a3f03e9834c60c
SimonC03 <85081292+SimonC03@users.noreply.github.com>
2024-07-18 09:46:54 +0200

    Update Footer.tsx

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -18,9 +18,9 @@ const Footer = () => {
           </div>
         </Link>
        <div className="trustpilot-widget" data-locale="en-US" data-template-id="56278e9abfbbba0bdcd568bc" data-businessunit-id="6698a6bb4f3dfbc3a06f8906" data-style-height="25px" data-style-width="50%">
-          <div href="https://www.trustpilot.com/review/stunote.se" target="_blank" rel="noopener">
+          <a href="https://www.trustpilot.com/review/stunote.se" target="_blank" rel="noopener">
             <Image src="/icons/trustpilotLogo.svg" width={25} height={25} alt="Trustpilot Icon" />
-          </div>
+          </a>
       </div>
       </div>
       <div className="text-xxs sm:text-xs md:text-sm text-white">
