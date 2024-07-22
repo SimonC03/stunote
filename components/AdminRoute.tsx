b96897a3d61fe147bc836f2c22d91055a5febc32
SimonC03 <blixxtensimon@gmail.com>
2024-07-22 17:06:31 +0200

    update

--- components/AdminRoute.tsx
+++ components/AdminRoute.tsx
@@ -3,31 +3,38 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { getUserRole } from '@/lib/appwrite';
+import '@/components/animations/spinner.css';
 
 const AdminRoute = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const [isAdmin, setIsAdmin] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkAdminRole = async () => {
       try {
-        const role = await getUserRole();
+        const roles = await getUserRole();
+        setIsAdmin(roles.includes('admin'));
 
-        if (role !== 'admin') {
+        if (!roles.includes('admin')) {
           router.push('/'); // Omdirigera om användaren inte är admin
-        } else {
-          setIsAdmin(true);
         }
       } catch (error) {
         router.push('/sign-in');
+      } finally {
+        setLoading(false);
       }
     };
 
     checkAdminRole();
   }, [router]);
 
+  if (loading) {
+    return <div className="spinner" style={{ borderRightColor: '#2268CD' }}></div>; // Visa en laddningsindikator medan vi väntar på att kontrollera rollen
+  }
+
   if (!isAdmin) {
-    return null; // Visa inget medan vi väntar på att kontrollera rollen
+    return null; // Visa inget om användaren inte är admin
   }
 
   return <>{children}</>;
