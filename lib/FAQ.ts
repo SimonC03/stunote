4f3cdc44bb259e70925ca635c233e42c449d78f7
SimonC03 <blixxtensimon@gmail.com>
2024-07-22 18:07:30 +0200

    fix

--- lib/FAQ.ts
+++ lib/FAQ.ts
@@ -16,5 +16,9 @@ export const FAQ_DATA = [
   {
     question: "Is StuNote free to use?",
     answer: "Currently, StuNote is free for all users. In the future, we will offer both a free tier and a premium subscription."
+  },
+  {
+    question: "How can i delete my account?",
+    answer: "Send us a massage on the 'Contact Us' tab"  
   }
 ];
