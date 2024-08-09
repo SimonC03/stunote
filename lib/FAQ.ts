8a5183d7e11a15638f4e2822134db039b63b510c
SimonC03 <blixxtensimon@gmail.com>
2024-08-09 16:18:57 +0200

    faq

--- lib/FAQ.ts
+++ lib/FAQ.ts
@@ -1,5 +1,3 @@
-// lib/FAQ.ts
-
 export const FAQ_DATA = [
   {
     question: "Can I change my profile picture?",
@@ -18,7 +16,40 @@ export const FAQ_DATA = [
     answer: "Currently, StuNote is free for all users. In the future, we will offer both a free tier and a premium subscription."
   },
   {
-    question: "How can i delete my account?",
-    answer: "Send us a massage on the 'Contact Us' tab"
+    question: "How can I delete my account?",
+    answer: "Send us a message on the 'Contact Us' tab."
+  },
+
+  {
+    question: "Can I upload study materials?",
+    answer: "No, our student ambassadors upload the documents to keep the material in highest quality"
+  },
+  {
+    question: "What is the difference between a free account and a premium account?",
+    answer: "A free account gives you access to most features of StuNote. A premium account provides additional benefits like unlimited Quizzes, access to all documents, and free ad uploads."
+  },
+  {
+    question: "How can I reset my password?",
+    answer: "If you have forgotten your password, go to the login page and click on 'Forgot Password'. Follow the instructions to reset your password via email."
+  },
+  {
+    question: "Can I share my StuNote account with others?",
+    answer: "No, sharing your account with others is against our terms of service. Each user should have their own account."
+  },  
+  {
+    question: "How can I cancel my subscription?",
+    answer: "Send us a message on the 'Contact Us' tab"
+  },
+  {
+    question: "How do I report inappropriate content?",
+    answer: "If you come across content that you believe is inappropriate or violates our guidelines, you can contact us through the 'Contact Us' tab."
+  },
+  {
+    question: "What should I do if I encounter a bug or issue on the site?",
+    answer: "If you encounter any technical issues or bugs, please report them to our support team through the 'Contact Us' tab. Include as much detail as possible to help us resolve the issue quickly."
+  },
+  {
+    question: "Can I update or delete my uploaded materials?",
+    answer: "Yes, you can update or delete any materials you have uploaded by sending us a message through the 'Contact Us' tab."
   }
 ];
